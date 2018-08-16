33901596101668dfd3ec627e8856767364efa9aa
johnhckuo <johnhckuo@gmail.com>
2018-08-16 17:27:31 +0800

    enable creating account

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
-const test = () => 'hello world';
-console.log(test());
+const StellarSdk = require('stellar-sdk');
+const request = require('request');
+
+const pair = StellarSdk.Keypair.random();
+const secret = pair.secret();
+const publicKey = pair.publicKey();
+console.log(secret, publicKey);
+createTestAccount();
+
+function createTestAccount() {
+  request.get({
+    url: 'https://friendbot.stellar.org',
+    qs: { addr: pair.publicKey() },
+    json: true,
+  }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.error('ERROR!', error || body);
+    } else {
+      console.log('SUCCESS! You have a new account :)\n', body);
+    }
+  });
+}
