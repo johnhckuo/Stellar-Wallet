dedc5159c56a2a8a641fe1fd20ad16ee7e79f4a9
Johnhckuo <johnhckuo@Calhoun5.local>
2018-08-24 22:52:44 +0800

    creating sender/receiver account

---
+++ src/utils.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const StellarSdk = require('stellar-sdk');
+
+export default class Account{
+  constructor(publicKey){
+    this.publicKey = publicKey;
+  }
+
+  createTestAccount() {
+    return new Promise((resolve, reject)=>{
+      request.get({
+        url: 'https://friendbot.stellar.org',
+        qs: { addr: this.publicKey },
+        json: true,
+      }, (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          console.error('ERROR!', error || body);
+          reject();
+        } else {
+          console.log('SUCCESS! Sending account :)\n', body);
+          resolve();
+        }
+      });
+    })
+  }
+
+  getAccountDetail() {
+    const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+    server.loadAccount(this.publicKey).then((account) => {
+      console.log(`Balances for account: ${this.publicKey}`);
+      account.balances.forEach((balance) => {
+        console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
+      });
+    });
+  }
+}
